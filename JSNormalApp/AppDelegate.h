a7381c398ee7257d9869c4278b79dcddb8f82264
niugaifang <niugaifang@juesheng.com>
2016-06-02 14:59:32 +0800

    Initial Commit

---
+++ JSNormalApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSNormalApp
+//
+//  Created by DaisyNew on 16/6/2.
+//  Copyright © 2016年 DaisyNew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
